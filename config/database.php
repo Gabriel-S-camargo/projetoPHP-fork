0dfa3b6c02028c8f9b792f46420facdbc6082d02
Gabriel Santos <gabrielsantoscamargo8@gmail.com>
2024-05-20 17:23:12 -0300

    ajuste de seta no lugar do igual

--- config/database.php
+++ config/database.php
@@ -4,7 +4,7 @@
 // Revisa para saber onde colocar para ser o primeiro codigo sql a ser executado 
 
 $BdCreate = "CREATE DATABASE IF NOT EXISTS LogusDB; USE LogusDB;";
-$BdCreation->$pdo->prepare($BdCreate);
+$BdCreation = $pdo->prepare($BdCreate);
 $BdCreation->execute();
 
 ?>
